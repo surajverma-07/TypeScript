229b084fb2242d85f48caabb10716246e3e82e77
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-07-18 23:00:37 +0530

    variables

---
+++ 01INTRO/variable.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello Suraj";
+var myNum = 7;
+// myNum.toUpperCase() 
+greetings.toLowerCase();
+console.log(greetings);
---
+++ 01INTRO/variable.ts
@@ -0,0 +1,13 @@
+let greetings: string = "Hello Suraj"
+let myNum = 7;
+// myNum.toUpperCase() 
+greetings.toLowerCase();
+console.log(greetings)
+
+//number 
+let userId:number = 345
+
+//boolean
+let isLoggedIn:boolean = false;
+
+export {}
