0e3f2f8304ef2c31bcc9d576948d5f748c85c7ba
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-07-20 21:12:09 +0530

    return type in object , type keyword use to create user defined type

--- 02BASIC/objects.ts
+++ 02BASIC/objects.ts
@@ -1,21 +1,39 @@
-const User = {
-    name:"suraj",
-    email:"suraj@gmail.com",
-    isActive:true 
+// const User = {
+//     name:"suraj",
+//     email:"suraj@gmail.com",
+//     isActive:true 
+// }
+
+// //parameter type should be same fn({property} then while calling it should be fn({property}))
+// function createUser({name:string,isPaid:boolean = false}){}
+
+// createUser({name:"suraj",isPaid:false});
+
+// //return type checking 
+// function createCourse():{name:string,price:number}{
+// //   return ;//it is wrong 
+//     // return {};// it is also wrong
+//     return {name:"jay Shree Ram",price:1000};
+// }
+// createCourse();
+
+
+type User = {
+    name:string;
+    email:string;
+    isActive:boolean;
 }
 
-//parameter type should be same fn({property} then while calling it should be fn({property}))
-function createUser({name:string,isPaid:boolean = false}){}
-
-createUser({name:"suraj",isPaid:false});
-
-//return type checking 
-function createCourse():{name:string,price:number}{
-//   return ;//it is wrong 
-    // return {};// it is also wrong
-    return {name:"jay Shree Ram",price:1000};
+//we can declare a type (user defained ) and use like this 
+function createUser(user:User):User{
+    // return {};
+    return user;
 }
-createCourse();
+
+// createUser({});//gives an error bcz we have to provide proper type of it
+createUser({name:"suraj",email:"suraj",isActive:true});
+
+
 
 
 
