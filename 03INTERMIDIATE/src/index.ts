d5085841cde12f72e31d9a503da21646555ccdf8
SURAJ KUMAR VERMA <137256443+surajverma-07@users.noreply.github.com>
2024-07-25 22:49:25 +0530

    debugging

--- 03INTERMIDIATE/src/index.ts
+++ 03INTERMIDIATE/src/index.ts
@@ -2,7 +2,7 @@ class User {
     email:string;
     name:string;
     readonly city:string="Raipur";
-
+     
     constructor(email:string,name:string){
         this.email = email;
         this.name= name;
